db29adf97fa7675d8fd8f87dd78f3adb2b2b5926
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 17:48:02 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,31 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 
-const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
-const chatId = '-943696838';
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const chatId = 'YOUR_TELEGRAM_CHAT_ID';
 
 const bot = new TelegramBot(token, { polling: false });
 
-function sendToTelegram(message) {
-  bot.sendMessage(chatId, message);
+function sendTelegramMessage(message) {
+  bot.sendMessage(chatId, message)
+    .then(() => {
+      console.log('Message sent to Telegram');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-function analyzePrice() {
-  const prices = JSON.parse(fs.readFileSync('price.json'));
-  const trend = calculateTrend(prices);
-  const levels = calculateLevels(prices);
-  const message = `Тренд: ${trend}\nУровни поддержки: ${levels.support}\nУровни сопротивления: ${levels.resistance}`;
-  sendToTelegram(message);
-}
-
-analyzePrice();
+fs.readFile('price.json', (err, data) => {
+  if (err) throw err;
+  const prices = JSON.parse(data);
+  const maxPrice = Math.max(...prices);
+  const minPrice = Math.min(...prices);
+  const currentPrice = prices[prices.length - 1];
+  const range = maxPrice - minPrice;
+  const support = minPrice + range * 0.25;
+  const resistance = maxPrice - range * 0.25;
+  const trend = currentPrice > prices[prices.length - 2] ? 'up' : 'down';
+  const message = `Текущая цена: ${currentPrice.toFixed(2)}\Поддержка : ${support.toFixed(2)}\nСопротивление: ${resistance.toFixed(2)}\nТренд: ${trend}`;
+  sendTelegramMessage(message);
+});
