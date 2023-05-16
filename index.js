0033b025f9b14e2602701095d7c0d14a7a2bdfad
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 20:37:23 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const { spawn } = require('child_process');
 
-const loadPriceInterval = 5000; // 5 секунд
-const botInterval = 60000; // 1 минута
+const loadPriceProcess = spawn('node', ['loadprice.js']);
+const botProcess = spawn('node', ['bot.js']);
 
-// Запускаем скрипт loadprice.js с интервалом loadPriceInterval
 setInterval(() => {
-  const childLoadPrice = spawn('node', ['loadprice.js']);
+  loadPriceProcess.kill();
+  loadPriceProcess = spawn('node', ['loadprice.js']);
+}, 5000);
 
-  childLoadPrice.stdout.on('data', data => {
-    console.log(`stdout: ${data}`);
-  });
-
-  childLoadPrice.stderr.on('data', data => {
-    console.error(`stderr: ${data}`);
-  });
-
-  childLoadPrice.on('close', code => {
-    console.log(`loadprice.js exited with code ${code}`);
-  });
-}, loadPriceInterval);
-
-// Запускаем скрипт bot.js с интервалом botInterval
 setInterval(() => {
-  const childBot = spawn('node', ['bot.js']);
-
-  childBot.stdout.on('data', data => {
-    console.log(`stdout: ${data}`);
-  });
-
-  childBot.stderr.on('data', data => {
-    console.error(`stderr: ${data}`);
-  });
-
-  childBot.on('close', code => {
-    console.log(`bot.js exited with code ${code}`);
-  });
-}, botInterval);
+  botProcess.kill();
+  botProcess = spawn('node', ['bot.js']);
+}, 60000);
