6020c1f3e3622206a8c01ad48aa59585b8268d54
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 20:13:43 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const loadprice = require('./loadprice');
-const bot = require('./bot');
+const { spawn } = require('child_process');
 
-setInterval(() => {
-  loadprice();
-}, 5000);
+const loadPriceInterval = 5000; // 5 секунд
+const botInterval = 60000; // 1 минута
 
+// Запускаем скрипт loadprice.js с интервалом loadPriceInterval
 setInterval(() => {
-  bot();
-}, 60000);
+  const childLoadPrice = spawn('node', ['loadprice.js']);
+
+  childLoadPrice.stdout.on('data', data => {
+    console.log(`stdout: ${data}`);
+  });
+
+  childLoadPrice.stderr.on('data', data => {
+    console.error(`stderr: ${data}`);
+  });
+
+  childLoadPrice.on('close', code => {
+    console.log(`loadprice.js exited with code ${code}`);
+  });
+}, loadPriceInterval);
+
+// Запускаем скрипт bot.js с интервалом botInterval
+setInterval(() => {
+  const childBot = spawn('node', ['bot.js']);
+
+  childBot.stdout.on('data', data => {
+    console.log(`stdout: ${data}`);
+  });
+
+  childBot.stderr.on('data', data => {
+    console.error(`stderr: ${data}`);
+  });
+
+  childBot.on('close', code => {
+    console.log(`bot.js exited with code ${code}`);
+  });
+}, botInterval);
