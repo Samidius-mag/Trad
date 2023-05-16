65eea73b6c404369c637e7ad1696f68fd3fb9cad
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 19:10:44 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,29 +1,55 @@
 const fs = require('fs');
 
-const supportLevels = [0.9, 0.8, 0.7];
-const resistanceLevels = [1.1, 1.2, 1.3];
-
-function calculateTrend(prices) {
-  const currentPrice = prices[prices.length - 1];
-  const averagePrice = prices.reduce((sum, price) => sum + price, 0) / prices.length;
-  const trend = currentPrice > averagePrice ? 'up' : 'down';
-  return trend;
-}
+const SUPPORT_LEVELS = [0.9, 0.8, 0.7];
+const RESISTANCE_LEVELS = [1.1, 1.2, 1.3];
 
 function calculateLevels(prices) {
   const currentPrice = prices[prices.length - 1];
-  const support = supportLevels.map(level => currentPrice * level);
-  const resistance = resistanceLevels.map(level => currentPrice * level);
-  return { support, resistance };
+  const supportLevels = SUPPORT_LEVELS.map(level => level * currentPrice);
+  const resistanceLevels = RESISTANCE_LEVELS.map(level => level * currentPrice);
+
+  return {
+    currentPrice,
+    supportLevels,
+    resistanceLevels
+  };
 }
 
-function analyzePrice() {
-  const prices = JSON.parse(fs.readFileSync('price.json'));
-  const trend = calculateTrend(prices);
+function calculateTrend(prices) {
+  const currentPrice = prices[prices.length - 1];
+  const previousPrice = prices[prices.length - 2];
+
+  if (currentPrice > previousPrice) {
+    return 'Восходящий';
+  } else if (currentPrice < previousPrice) {
+    return 'Нисходящий';
+  } else {
+    return 'Боковой';
+  }
+}
+
+function calculateActivity(prices) {
+  const currentPrice = prices[prices.length - 1];
+  const minPrice = Math.min(...prices);
+  const maxPrice = Math.max(...prices);
+
+  const activity = ((currentPrice - minPrice) / (maxPrice - minPrice)) * 100;
+
+  return activity.toFixed(2);
+}
+
+fs.readFile('price.json', (err, data) => {
+  if (err) throw err;
+
+  const prices = JSON.parse(data).map(candle => parseFloat(candle.close));
+
   const levels = calculateLevels(prices);
-  console.log(`Trend: ${trend}`);
-  console.log(`Support levels: ${levels.support}`);
-  console.log(`Resistance levels: ${levels.resistance}`);
-}
+  const trend = calculateTrend(prices);
+  const activity = calculateActivity(prices);
 
-analyzePrice();
+  console.log('Текущая цена:', levels.currentPrice);
+  console.log('Уровни поддержки:', levels.supportLevels);
+  console.log('Уровни сопротивления:', levels.resistanceLevels);
+  console.log('Направление тренда:', trend);
+  console.log('Активность на рынке:', activity, '%');
+});
