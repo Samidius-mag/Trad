44b01380a727d2023b669fde5b8d9be121348542
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 16:12:15 +1000

    Update loadprice.js

--- loadprice.js
+++ loadprice.js
@@ -1,28 +1,25 @@
 const https = require('https');
 const fs = require('fs');
 
-const symbol = 'BTCUSDT';
-const interval = '1h';
+const interval = 20000; // 20 seconds
 const limit = 5000;
-const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
+const symbol = 'BTCUSDT';
 
 function loadPrice() {
-  https.get(url, (res) => {
+  https.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=1h&limit=${limit}`, (res) => {
     let data = '';
     res.on('data', (chunk) => {
       data += chunk;
     });
     res.on('end', () => {
       const candles = JSON.parse(data);
-      const prices = candles.map((candle) => parseFloat(candle[4]));
-      fs.writeFile('price.json', JSON.stringify(prices), (err) => {
-        if (err) throw err;
-        console.log('Price data saved to price.json');
-      });
+      const prices = candles.map(candle => parseFloat(candle[4]));
+      fs.writeFileSync('price.json', JSON.stringify(prices));
     });
   }).on('error', (err) => {
     console.error(err);
   });
 }
 
-setInterval(loadPrice, 20000);
+loadPrice();
+setInterval(loadPrice, interval);
