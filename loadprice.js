2db28e9b057cd530f3b3f39b1c1af870a0e9048b
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-17 07:55:06 +1000

    Update loadprice.js

--- loadprice.js
+++ loadprice.js
@@ -1,28 +1,24 @@
-const axios = require('axios');
+const https = require('https');
 const fs = require('fs');
 
 const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=5000';
 
-axios.get(url)
-  .then(response => {
-    const data = response.data.map(candle => ({
-      time: candle[0],
-      open: candle[1],
-      high: candle[2],
-      low: candle[3],
-      close: candle[4],
-      volume: candle[5],
-      closeTime: candle[6],
-      quoteAssetVolume: candle[7],
-      numberOfTrades: candle[8],
-      takerBuyBaseAssetVolume: candle[9],
-      takerBuyQuoteAssetVolume: candle[10],
-    }));
-    fs.writeFile('price.json', JSON.stringify(data), err => {
-      if (err) throw err;
-      console.log('записано');
-    });
-  })
-  .catch(error => {
-    console.log(error);
+https.get(url, (res) => {
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
   });
+
+  res.on('end', () => {
+    const candles = JSON.parse(data);
+    const prices = candles.map(candle => parseFloat(candle[4]));
+    fs.writeFile('price.json', JSON.stringify(prices), (err) => {
+      if (err) throw err;
+      console.log('Записано');
+    });
+  });
+
+}).on('error', (err) => {
+  console.log('Ошибка: ' + err.message);
+});
