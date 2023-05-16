a888051c9f09ef83894438912788f0bfe1d0735f
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-17 08:16:57 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,24 @@
+const { exec } = require('child_process');
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-const { calculateLevels, calculateTrend, calculateActivity } = require('./logic');
 
 const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
 const chatId = '-943696838';
 
 const bot = new TelegramBot(token, { polling: false });
 
-function sendMessage(message) {
-  bot.sendMessage(chatId, message)
-    .then(() => {
-      console.log('отправлено');
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+const sendMessage = (message) => {
+  bot.sendMessage(chatId, message);
+};
 
-fs.readFile('price.json', (err, data) => {
-  if (err) throw err;
-  const prices = JSON.parse(data).map(candle => parseFloat(candle.close));
-  const currentPrice = prices[prices.length - 1];
-  const { supportLevels, resistanceLevels } = calculateLevels(prices);
-  const trend = calculateTrend(prices);
-  const activity = calculateActivity(prices);
-  const message = `Текущая цена: ${currentPrice}\nУровни поддержки: ${supportLevels.join(', ')}\nУровни сопротивления: ${resistanceLevels.join(', ')}\nНаправление тренда: ${trend}\nАктивность на рынке: ${activity}%`;
+exec('node logic.js', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+
+  const lines = stdout.trim().split('\n');
+  const message = lines.join('\n');
+
   sendMessage(message);
+  console.log('Отправлено');
 });
