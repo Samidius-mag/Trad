b67a60735c9fe6afb1039a4e42ee6d79707d87c0
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 15:55:07 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { spawn } = require('child_process');
 
 function calculateSupportResistance(prices) {
   const maxPrice = Math.max(...prices);
@@ -33,26 +32,12 @@ function calculateTrend(prices) {
   }
 }
 
-function runLogic() {
-  fs.readFile('price.json', (err, data) => {
-    if (err) throw err;
-    const prices = JSON.parse(data);
-    const levels = calculateSupportResistance(prices);
-    const trend = calculateTrend(prices);
-    console.log(`Support levels: ${levels.support1}, ${levels.support2}, ${levels.support3}`);
-    console.log(`Resistance levels: ${levels.resistance1}, ${levels.resistance2}, ${levels.resistance3}`);
-    console.log(`Trend: ${trend}`);
-    setTimeout(() => {
-      console.log('Restarting script...');
-      const logic = spawn('node', ['logic.js']);
-      logic.stdout.on('data', (data) => {
-        console.log(`logic.js: ${data}`);
-      });
-      logic.stderr.on('data', (data) => {
-        console.error(`logic.js error: ${data}`);
-      });
-    }, 120000);
-  });
-}
-
-runLogic();
+fs.readFile('price.json', (err, data) => {
+  if (err) throw err;
+  const prices = JSON.parse(data);
+  const levels = calculateSupportResistance(prices);
+  const trend = calculateTrend(prices);
+  console.log(`Support levels: ${levels.support1}, ${levels.support2}, ${levels.support3}`);
+  console.log(`Resistance levels: ${levels.resistance1}, ${levels.resistance2}, ${levels.resistance3}`);
+  console.log(`Trend: ${trend}`);
+});
