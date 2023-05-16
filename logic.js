81c10197bd0240b7d7576e80d53cd76e0bccb602
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-17 08:14:20 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,20 +1,132 @@
 const fs = require('fs');
 
-const prices = JSON.parse(fs.readFileSync('price.json'));
+const calculateMarketActivity = (candles) => {
+  const averageVolume = candles.reduce((sum, candle) => sum + candle.volume, 0) / candles.length;
+  const currentVolume = candles[candles.length - 1].volume;
+  const activity = currentVolume / averageVolume * 100;
+  return activity;
+};
 
-const monthPrices = prices.slice(-720); // 720 часовых свечей = месяц
+const calculateMarketVolatility = (candles) => {
+  const maxHigh = Math.max(...candles.map(candle => candle.high));
+  const minLow = Math.min(...candles.map(candle => candle.low));
+  const volatility = (maxHigh - minLow) / maxHigh * 100;
+  return volatility;
+};
 
-const average = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+const calculateSupportResistanceLevels = (candles) => {
+  const monthCandles = candles.slice(-720);
+  const monthHigh = Math.max(...monthCandles.map(candle => candle.high));
+  const monthLow = Math.min(...monthCandles.map(candle => candle.low));
+  const monthRange = monthHigh - monthLow;
+  const monthSupport = monthLow + monthRange * 0.382;
+  const monthResistance = monthLow + monthRange * 0.618;
 
-const currentPrice = prices[prices.length - 1];
-const monthAverage = average(monthPrices);
-const trend = currentPrice > monthAverage ? 'восходящий' : currentPrice < monthAverage ? 'нисходящий' : 'боковой';
-const volatility = Math.round(100 * (Math.max(...prices) - Math.min(...prices)) / monthAverage);
-const support = Math.min(...monthPrices);
-const resistance = Math.max(...monthPrices);
+  const dayCandles = candles.slice(-24);
+  const dayHigh = Math.max(...dayCandles.map(candle => candle.high));
+  const dayLow = Math.min(...dayCandles.map(candle => candle.low));
+  const dayRange = dayHigh - dayLow;
+  const daySupport = dayLow + dayRange * 0.382;
+  const dayResistance = dayLow + dayRange * 0.618;
 
-console.log(`Текущая цена: ${currentPrice}`);
-console.log(`Текущий тренд: ${trend}`);
-console.log(`Волатильность: ${volatility}%`);
-console.log(`Уровень поддержки: ${support}`);
-console.log(`Уровень сопротивления: ${resistance}`);
+  const currentCandle = candles[candles.length - 1];
+  const currentRange = currentCandle.high - currentCandle.low;
+  const currentSupport = currentCandle.low + currentRange * 0.382;
+  const currentResistance = currentCandle.low + currentRange * 0.618;
+
+  return {
+    monthSupport,
+    monthResistance,
+    daySupport,
+    dayResistance,
+    currentSupport,
+    currentResistance,
+  };
+};
+
+const calculateTrend = (candles) => {
+  const activity = calculateMarketActivity(candles);
+  const trend = {
+    month: 'боковой',
+    day: 'боковой',
+    current: 'боковой',
+  };
+
+  if (activity >= 25) {
+    const monthCandles = candles.slice(-720);
+    const monthHigh = Math.max(...monthCandles.map(candle => candle.high));
+    const monthLow = Math.min(...monthCandles.map(candle => candle.low));
+    const monthClose = monthCandles[monthCandles.length - 1].close;
+    const monthRange = monthHigh - monthLow;
+    const monthMiddle = monthLow + monthRange * 0.5;
+
+    if (monthClose > monthMiddle) {
+      trend.month = 'восходящий';
+    } else if (monthClose < monthMiddle) {
+      trend.month = 'нисходящий';
+    }
+
+    const dayCandles = candles.slice(-24);
+    const dayHigh = Math.max(...dayCandles.map(candle => candle.high));
+    const dayLow = Math.min(...dayCandles.map(candle => candle.low));
+    const dayClose = dayCandles[dayCandles.length - 1].close;
+    const dayRange = dayHigh - dayLow;
+    const dayMiddle = dayLow + dayRange * 0.5;
+
+    if (dayClose > dayMiddle) {
+      trend.day = 'восходящий';
+    } else if (dayClose < dayMiddle) {
+      trend.day = 'нисходящий';
+    }
+
+    const currentCandle = candles[candles.length - 1];
+    const currentHigh = currentCandle.high;
+    const currentLow = currentCandle.low;
+    const currentClose = currentCandle.close;
+    const currentRange = currentHigh - currentLow;
+    const currentMiddle = currentLow + currentRange * 0.5;
+
+    if (currentClose > currentMiddle) {
+      trend.current = 'восходящий';
+    } else if (currentClose < currentMiddle) {
+      trend.current = 'нисходящий';
+    }
+  }
+
+  return trend;
+};
+
+const calculateCurrentPrice = (candles) => {
+  const currentPrice = candles[candles.length - 1].close;
+  return currentPrice;
+};
+
+fs.readFile('price.json', (err, data) => {
+  if (err) throw err;
+  const candles = JSON.parse(data);
+
+  const activity = calculateMarketActivity(candles);
+  const volatility = calculateMarketVolatility(candles);
+  const supportResistanceLevels = calculateSupportResistanceLevels(candles);
+  const trend = calculateTrend(candles);
+  const currentPrice = calculateCurrentPrice(candles);
+
+  console.log('Активность рынка:', activity.toFixed(2), '%');
+  console.log('Волатильность рынка:', volatility.toFixed(2), '%');
+  console.log('Уровни поддержки и сопротивления:');
+  console.log('Месячный период:');
+  console.log('Поддержка:', supportResistanceLevels.monthSupport.toFixed(2));
+  console.log('Сопротивление:', supportResistanceLevels.monthResistance.toFixed(2));
+  console.log('Текущий день:');
+  console.log('Поддержка:', supportResistanceLevels.daySupport.toFixed(2));
+  console.log('Сопротивление:', supportResistanceLevels.dayResistance.toFixed(2));
+  console.log('Текущий период:');
+  console.log('Поддержка:', supportResistanceLevels.currentSupport.toFixed(2));
+  console.log('Сопротивление:', supportResistanceLevels.currentResistance.toFixed(2));
+  console.log('Месячный тренд:', trend.month);
+  console.log('Дневной тренд:', trend.day);
+  console.log('Текущий тренд:', trend.current);
+  console.log('Текущая цена:', currentPrice.toFixed(2));
+  console.log('Активность и волатильность в %:');
+  console.log(activity.toFixed(2), '%', volatility.toFixed(2), '%');
+});
