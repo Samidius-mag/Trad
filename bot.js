e87d85175fc1dd0360b6c41848b9c30e7a6e1158
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-16 16:16:07 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,21 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 
-// Задаем токен бота, полученный от BotFather
 const token = '5995075949:AAHek1EL2dqZvJlIR3ssuFLkIsb3ZTgccIQ';
-
-// Создаем экземпляр бота
-const bot = new TelegramBot(token, { polling: false });
-
-// Задаем ID чата, в который будем отправлять сообщения
 const chatId = '-943696838';
 
-// Функция для отправки сообщения в чат
-function sendMessage(message) {
+const bot = new TelegramBot(token, { polling: false });
+
+function sendToTelegram(message) {
   bot.sendMessage(chatId, message);
 }
 
-// Функция для чтения данных из файла logic.js и отправки сообщения в чат
-function sendLogicData() {
-  fs.readFile('logic.js', (err, data) => {
-    if (err) throw err;
-    const message = data.toString();
-    sendMessage(message);
-  });
+function analyzePrice() {
+  const prices = JSON.parse(fs.readFileSync('price.json'));
+  const trend = calculateTrend(prices);
+  const levels = calculateLevels(prices);
+  const message = `Trend: ${trend}\nSupport levels: ${levels.support}\nResistance levels: ${levels.resistance}`;
+  sendToTelegram(message);
 }
 
-// Отправляем сообщение сразу после запуска бота
-sendLogicData();
-
-// Отправляем сообщение каждые 5 минут
-setInterval(sendLogicData, 300000);
+analyzePrice();
