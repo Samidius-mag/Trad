ba7f12cec43272ce1a1db1cccf1dacb532711948
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-17 18:43:47 +1000

    Update loadprice.js

--- loadprice.js
+++ loadprice.js
@@ -1,31 +1,26 @@
-const https = require('https');
+const axios = require('axios');
 const fs = require('fs');
 
-const symbol = 'BTCUSDT';
-const interval = '1h';
-const limit = 5000;
+const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=8760';
 
-const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
-
-https.get(url, (res) => {
-  let data = '';
-
-  res.on('data', (chunk) => {
-    data += chunk;
+axios.get(url)
+  .then(response => {
+    const data = response.data.map(candle => ({
+      time: candle[0],
+      open: parseFloat(candle[1]),
+      high: parseFloat(candle[2]),
+      low: parseFloat(candle[3]),
+      close: parseFloat(candle[4]),
+      volume: parseFloat(candle[5]),
+      closeTime: candle[6],
+      quoteAssetVolume: parseFloat(candle[7]),
+      numberOfTrades: candle[8],
+      takerBuyBaseAssetVolume: parseFloat(candle[9]),
+      takerBuyQuoteAssetVolume: parseFloat(candle[10])
+    }));
+    fs.writeFileSync('price.json', JSON.stringify(data));
+    console.log('Data saved to price.json');
+  })
+  .catch(error => {
+    console.log(error);
   });
-
-  res.on('end', () => {
-    const candles = JSON.parse(data);
-
-    const prices = candles.map((candle) => {
-      return parseFloat(candle[4]);
-    });
-
-    fs.writeFile('price.json', JSON.stringify(prices), (err) => {
-      if (err) throw err;
-      console.log('записано');
-    });
-  });
-}).on('error', (err) => {
-  console.log('Ошибка: ' + err.message);
-});
