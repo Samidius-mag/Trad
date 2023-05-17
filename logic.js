bd5a1b4febb434ccd8faf5de64c6a057005afddf
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-17 18:44:43 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,46 +1,139 @@
 const fs = require('fs');
-const { ROC } = require('technicalindicators');
+const { EMA } = require('technicalindicators');
 
-fs.readFile('price.json', (err, data) => {
-  if (err) throw err;
+const data = JSON.parse(fs.readFileSync('price.json'));
 
-  const prices = JSON.parse(data);
+// определение скорости изменения цены
+const priceChangeSpeed = (priceArray, hours) => {
+  const priceNow = priceArray[priceArray.length - 1].close;
+  const priceThen = priceArray[priceArray.length - 1 - hours].close;
+  return (priceNow - priceThen) / priceThen;
+};
 
-  const roc = ROC.calculate({ period: 14, values: prices });
+// относительный индекс волатильности
+const relativeVolatilityIndex = (priceArray, hours) => {
+  const priceHigh = Math.max(...priceArray.slice(-hours).map(candle => candle.high));
+  const priceLow = Math.min(...priceArray.slice(-hours).map(candle => candle.low));
+  const priceRange = priceHigh - priceLow;
+  const priceClose = priceArray[priceArray.length - 1].close;
+  return (priceClose - priceLow) / priceRange;
+};
 
-  const rvi = roc.reduce((sum, value) => {
-    return sum + value;
-  }, 0) / roc.length;
+// линейный регрессионный канал
+const linearRegressionChannel = (priceArray, hours) => {
+  const prices = priceArray.slice(-hours).map(candle => candle.close);
+  const x = prices.map((price, index) => index);
+  const sumX = x.reduce((a, b) => a + b, 0);
+  const sumY = prices.reduce((a, b) => a + b, 0);
+  const sumXY = x.reduce((a, b, index) => a + b * prices[index], 0);
+  const sumX2 = x.reduce((a, b) => a + b ** 2, 0);
+  const n = prices.length;
+  const slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX ** 2);
+  const intercept = (sumY - slope * sumX) / n;
+  const upperChannel = prices.map((price, index) => slope * index + intercept + 2 * priceRange(prices, index));
+  const lowerChannel = prices.map((price, index) => slope * index + intercept - 2 * priceRange(prices, index));
+  return { upperChannel, lowerChannel };
+};
 
-  let trend = '';
-  if (rvi > 0.5) {
-    trend = 'восходящий';
-  } else if (rvi < -0.5) {
-    trend = 'нисходящий';
+// функция для вычисления ценового диапазона
+const priceRange = (prices, index) => {
+  const pricesInRange = prices.slice(Math.max(0, index - 20), index + 1);
+  const priceHigh = Math.max(...pricesInRange);
+  const priceLow = Math.min(...pricesInRange);
+  return priceHigh - priceLow;
+};
+
+// EMA 21, 55, 89, 144, 233
+const ema21 = EMA.calculate({ period: 21, values: data.map(candle => candle.close) });
+const ema55 = EMA.calculate({ period: 55, values: data.map(candle => candle.close) });
+const ema89 = EMA.calculate({ period: 89, values: data.map(candle => candle.close) });
+const ema144 = EMA.calculate({ period: 144, values: data.map(candle => candle.close) });
+const ema233 = EMA.calculate({ period: 233, values: data.map(candle => candle.close) });
+
+// направление тренда
+const trendDirection = (priceArray, hours) => {
+  const priceNow = priceArray[priceArray.length - 1].close;
+  const priceThen = priceArray[priceArray.length - 1 - hours].close;
+  if (priceNow > priceThen) {
+    return 'восходящий';
+  } else if (priceNow < priceThen) {
+    return 'нисходящий';
   } else {
-    trend = 'боковой';
+    return 'боковой';
   }
+};
 
-  const support = Math.min(...prices);
-  const resistance = Math.max(...prices);
+// уровни поддержки и сопротивления
+const supportResistanceLevels = (priceArray, hours) => {
+  const prices = priceArray.slice(-hours).map(candle => candle.close);
+  const priceHigh = Math.max(...prices);
+  const priceLow = Math.min(...prices);
+  const priceRange = priceHigh - priceLow;
+  const support1 = priceLow + 0.236 * priceRange;
+  const support2 = priceLow + 0.382 * priceRange;
+  const support3 = priceLow + 0.618 * priceRange;
+  const resistance1 = priceHigh - 0.236 * priceRange;
+  const resistance2 = priceHigh - 0.382 * priceRange;
+  const resistance3 = priceHigh - 0.618 * priceRange;
+  return { support1, support2, support3, resistance1, resistance2, resistance3 };
+};
 
-  const monthlyTrend = prices[0] < prices[prices.length - 1] ? 'восходящий' : 'нисходящий';
-  const dailyTrend = prices[prices.length - 2] < prices[prices.length - 1] ? 'восходящий' : 'нисходящий';
-  const currentTrend = prices[prices.length - 1] > prices[prices.length - 2] ? 'восходящий' : 'нисходящий';
+// возможные точки разворота рынка
+const possibleReversalPoints = (priceArray, hours) => {
+  const prices = priceArray.slice(-hours).map(candle => candle.close);
+  const priceHigh = Math.max(...prices);
+  const priceLow = Math.min(...prices);
+  const priceRange = priceHigh - priceLow;
+  const reversal1 = priceLow + 0.236 * priceRange;
+  const reversal2 = priceLow + 0.382 * priceRange;
+  const reversal3 = priceLow + 0.5 * priceRange;
+  const reversal4 = priceLow + 0.618 * priceRange;
+  const reversal5 = priceLow + 0.786 * priceRange;
+  const reversal6 = priceLow + priceRange;
+  return { reversal1, reversal2, reversal3, reversal4, reversal5, reversal6 };
+};
 
-  const currentPrice = prices[prices.length - 1];
+// текущая цена
+const currentPrice = data[data.length - 1].close;
 
-  const volatility = roc.reduce((sum, value) => {
-    return sum + Math.abs(value)*69;
-  }, 0) / roc.length;
+// текущая перекупленность/перепроданность рынка
+const currentOverboughtOversold = (priceArray, hours) => {
+  const prices = priceArray.slice(-hours).map(candle => candle.close);
+  const priceHigh = Math.max(...prices);
+  const priceLow = Math.min(...prices);
+  const priceRange = priceHigh - priceLow;
+  const priceClose = prices[prices.length - 1];
+  const overbought = (priceClose - priceLow) / priceRange;
+  const oversold = (priceHigh - priceClose) / priceRange;
+  return { overbought, oversold };
+};
 
-  console.log(`Тренд: ${trend}`);
-  console.log(`Уровень поддержки: ${support}`);
-  console.log(`Уровень сопротивления: ${resistance}`);
-  console.log(`Месячный тренд: ${monthlyTrend}`);
-  console.log(`Дневной тренд: ${dailyTrend}`);
-  console.log(`Текущий тренд: ${currentTrend}`);
-  console.log(`Текущая цена: ${currentPrice}`);
-  console.log(`Скорость движения рынка: ${rvi}`);
-  console.log(`Волатильность: ${volatility}`);
-});
+// рекомендация о покупке/продаже, или ожидании
+const recommendation = (priceArray, hours) => {
+  const priceNow = priceArray[priceArray.length - 1].close;
+  const priceThen = priceArray[priceArray.length - 1 - hours].close;
+  if (priceNow > priceThen) {
+    return 'покупать';
+  } else if (priceNow < priceThen) {
+    return 'продавать';
+  } else {
+    return 'ожидать';
+  }
+};
+
+module.exports = {
+  priceChangeSpeed,
+  relativeVolatilityIndex,
+  linearRegressionChannel,
+  ema21,
+  ema55,
+  ema89,
+  ema144,
+  ema233,
+  trendDirection,
+  supportResistanceLevels,
+  possibleReversalPoints,
+  currentPrice,
+  currentOverboughtOversold,
+  recommendation
+};
