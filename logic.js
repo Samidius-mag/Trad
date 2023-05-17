72ff93b16ebfceeebabf93fa4f9831617ab18049
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-18 08:45:10 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -65,18 +65,18 @@ const fib144 = calculateFibonacciLevels(prices, 144);
 const fib233 = calculateFibonacciLevels(prices, 233);
 const fib13 = calculateFibonacciLevels(prices, 13);
 const pivotPoints = [
-  { level: fib13[11], type: 'поддержка' },
-  { level: fib13[10], type: 'поддержка' },
-  { level: fib13[9], type: 'поддержка' },
-  { level: fib13[3], type: 'сопротивление' },
-  { level: fib13[2], type: 'сопротивление' },
-  { level: fib13[1], type: 'сопротивление' },
+  { level: fib13[11], type: 'под' },
+  { level: fib13[10], type: 'под' },
+  { level: fib13[9], type: 'под' },
+  { level: fib13[3], type: 'сопр' },
+  { level: fib13[2], type: 'сопр' },
+  { level: fib13[1], type: 'сопр' },
 ];
 const reversalPoints = [
-  { level: fib13[12], type: 'поддержка' },
-  { level: fib13[9], type: 'поддержка' },
-  { level: fib13[3], type: 'сопротивление' },
-  { level: fib13[0], type: 'сопротивление' },
+  { level: fib13[12], type: 'под' },
+  { level: fib13[9], type: 'под' },
+  { level: fib13[3], type: 'сопр' },
+  { level: fib13[0], type: 'сопр' },
 ];
 const rsi1h = calculateRSI(pricesClose, 14);
 const rsi4h = calculateRSI(pricesClose, 56);
@@ -108,18 +108,18 @@ console.log(`Рекомендация: ${recommendation}`);
 //console.log(EMA89: ${ema89.toFixed(2)});
 //console.log(EMA144: ${ema144.toFixed(2)});
 //console.log(EMA233: ${ema233.toFixed(2)});
-//console.log(SMA1h: ${sma1h.toFixed(2)} (${trend1h}));
-//console.log(SMA4h: ${sma4h.toFixed(2)} (${trend4h}));
-//console.log(SMA12h: ${sma12h.toFixed(2)} (${trend12h}));
-//console.log(SMA24h: ${sma24h.toFixed(2)} (${trend24h}));
+console.log(`SMA1h: ${sma1h.toFixed(2)} (${trend1h})`);
+console.log(`SMA4h: ${sma4h.toFixed(2)} (${trend4h})`);
+console.log(`SMA12h: ${sma12h.toFixed(2)} (${trend12h})`);
+console.log(`SMA24h: ${sma24h.toFixed(2)} (${trend24h})`);
 //console.log(Fibonacci 21: ${fib21.join(', ')});
 //console.log(Fibonacci 55: ${fib55.join(', ')});
 //console.log(Fibonacci 89: ${fib89.join(', ')});
 //console.log(Fibonacci 144: ${fib144.join(', ')});
 //console.log(Fibonacci 233: ${fib233.join(', ')});
-console.log(`Pivot Points: ${pivotPoints.map(point => `${point.type} ${point.level}`).join(', ')}`);
-console.log(`Reversal Points: ${reversalPoints.map(point => `${point.level}`).join(', ')}`);
-console.log(`RSI1h: ${rsi1h.toFixed(2)} (${oversold1h ? 'oversold' : overbought1h ? 'overbought' : 'neutral'})`);
-console.log(`RSI4h: ${rsi4h.toFixed(2)} (${oversold4h ? 'oversold' : overbought4h ? 'overbought' : 'neutral'})`);
-console.log(`RSI12h: ${rsi12h.toFixed(2)} (${oversold12h ? 'oversold' : overbought12h ? 'overbought' : 'neutral'})`);
-console.log(`RSI24h: ${rsi24h.toFixed(2)} (${oversold24h ? 'oversold' : overbought24h ? 'overbought' : 'neutral'})`);
+console.log(`П/С: ${pivotPoints.map(point => `${point.type} ${point.level}`).join(', ')}`);
+console.log(`Разворот: ${reversalPoints.map(point => `${point.level}`).join(', ')}`);
+console.log(`RSI1h: ${rsi1h.toFixed(2)} (${oversold1h ? 'Перепродано' : overbought1h ? 'Перекупленно' : 'Нейтрально'})`);
+console.log(`RSI4h: ${rsi4h.toFixed(2)} (${oversold4h ? 'Перепродано' : overbought4h ? 'Перекупленно' : 'Нейтрально'})`);
+console.log(`RSI12h: ${rsi12h.toFixed(2)} (${oversold12h ? 'Перепродано' : overbought12h ? 'Перекупленно' : 'Нейтрально'})`);
+console.log(`RSI24h: ${rsi24h.toFixed(2)} (${oversold24h ? 'Перепродано' : overbought24h ? 'Перекупленно' : 'Нейтрально'})`);
