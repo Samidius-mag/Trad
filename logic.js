ef46e620be6a707d188fe8e32797eee1ce8cfc61
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-17 12:29:04 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,68 +1,58 @@
 const fs = require('fs');
-const { ROC, RVI } = require('technicalindicators');
+const { ROC } = require('technicalindicators');
 
-const data = JSON.parse(fs.readFileSync('price.json'));
+const data = fs.readFileSync('price.json');
+const prices = JSON.parse(data);
 
-const rocInput = {
-  values: data.map(candle => candle.close),
-  period: 14
-};
+const roc = ROC.calculate({ period: 12, values: prices });
 
-const rviInput = {
-  values: data.map(candle => ({
-    open: candle.open,
-    high: candle.high,
-    low: candle.low,
-    close: candle.close
-  })),
-  period: 14
-};
+const lastMonthPrices = prices.slice(-720);
+const lastMonthMax = Math.max(...lastMonthPrices);
+const lastMonthMin = Math.min(...lastMonthPrices);
+const lastMonthAvg = lastMonthPrices.reduce((a, b) => a + b, 0) / lastMonthPrices.length;
 
-const roc = ROC.calculate(rocInput);
-const rvi = RVI.calculate(rviInput);
+const todayPrices = prices.slice(-24);
+const todayMax = Math.max(...todayPrices);
+const todayMin = Math.min(...todayPrices);
+const todayAvg = todayPrices.reduce((a, b) => a + b, 0) / todayPrices.length;
 
-const lastCandle = data[data.length - 1];
-const currentPrice = lastCandle.close;
+const currentPrice = prices[prices.length - 1];
 
-const monthlyTrend = roc[roc.length - 30] > 0 ? 'Восходящий' : 'Нисходящий';
-const dailyTrend = roc[roc.length - 1] > 0 ? 'Восходящий' : 'Нисходящий';
-const currentTrend = roc[roc.length - 1] > roc[roc.length - 2] ? 'Восходящий' : 'Нисходящий';
+const trendSpeed = roc[roc.length - 1];
+const volatility = (lastMonthMax - lastMonthMin) / lastMonthAvg;
 
 let trend;
-if (Math.abs(roc[roc.length - 1]) < 1 && rvi[rvi.length - 1] >= 45 && rvi[rvi.length - 1] <= 55) {
-  trend = 'Боковой';
+if (trendSpeed > 1) {
+  trend = 'Восходящий';
+} else if (trendSpeed < -1) {
+  trend = 'Нисходящий';
 } else {
-  trend = currentTrend;
+  trend = 'Боковой';
 }
 
-const supportLevels = data.slice(-30).reduce((levels, candle) => {
-  if (candle.low < levels.support1 || !levels.support1) {
-    levels.support2 = levels.support1;
-    levels.support1 = candle.low;
-  } else if (candle.low < levels.support2 || !levels.support2) {
-    levels.support2 = candle.low;
-  }
-  return levels;
-}, { support1: null, support2: null });
+let support;
+if (currentPrice < todayMin) {
+  support = todayMin;
+} else if (currentPrice < lastMonthMin) {
+  support = lastMonthMin;
+} else {
+  support = null;
+}
 
-const resistanceLevels = data.slice(-30).reduce((levels, candle) => {
-  if (candle.high > levels.resistance1 || !levels.resistance1) {
-    levels.resistance2 = levels.resistance1;
-    levels.resistance1 = candle.high;
-  } else if (candle.high > levels.resistance2 || !levels.resistance2) {
-    levels.resistance2 = candle.high;
-  }
-  return levels;
-}, { resistance1: null, resistance2: null });
+let resistance;
+if (currentPrice > todayMax) {
+  resistance = todayMax;
+} else if (currentPrice > lastMonthMax) {
+  resistance = lastMonthMax;
+} else {
+  resistance = null;
+}
 
-const speed = roc[roc.length - 1];
-const volatility = rvi[rvi.length - 1];
-
-console.log('Текущая цена:', currentPrice);
-console.log('Месячный тренд:', monthlyTrend);
-console.log('Дневной тренд:', dailyTrend);
-console.log('Текущий тренд:', currentTrend);
-console.log('Уровни поддержки:', supportLevels);
-console.log('Уровни сопротивления:', resistanceLevels);
-console.log('Скорость движения рынка:', speed);
-console.log('Волатильность:', volatility);
+console.log({
+  trend,
+  support,
+  resistance,
+  currentPrice,
+  trendSpeed,
+  volatility,
+});
