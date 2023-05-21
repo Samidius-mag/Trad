110dcd5f395dd5691f84d03a4494f306c93e2eb7
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-21 17:56:09 +1000

    Update logic3.js

--- logic3.js
+++ logic3.js
@@ -23,8 +23,8 @@ const ma = SMA.calculate({ period: bbPeriod, values: prices.map(price => price.c
 const dev = bb.map(b => b.upper - b.lower);
 const forMult = 2;
 const sigmaDev = dev.map(d => d * sigma);
-const upper = ma.map((m, i) => m + sigmaDev[i]).toFixed(2);
-const lower = ma.map((m, i) => m - sigmaDev[i]).toFixed(2);
+const upper = ma.map((m, i) => m + sigmaDev[i]);
+const lower = ma.map((m, i) => m - sigmaDev[i]);
 
 // Сигнал на покупку
 const basis = EMA.calculate({ period: bbPeriod, values: rsi });
@@ -33,10 +33,10 @@ const buySignal = basis[basis.length - 1] + ((upper[upper.length - 1] - lower[lo
 // Сигнал на продажу
 const sellSignal = basis[basis.length - 1] - ((upper[upper.length - 1] - lower[lower.length - 1]) * sigma);
 
-console.log('Current price:', currentPrice);
-console.log('RSI:', rsi[rsi.length - 1]);
-console.log('Bollinger Bands:', bb[bb.length - 1]);
-console.log('Upper band:', upper[upper.length - 1]);
-console.log('Lower band:', lower[lower.length - 1]);
+console.log('Current price:', currentPrice.toFixed(2));
+console.log('RSI:', rsi[rsi.length - 1].toFixed(2));
+console.log('Bollinger Bands:', bb[bb.length - 1].toFixed(2));
+console.log('Upper band:', upper[upper.length - 1].toFixed(2));
+console.log('Lower band:', lower[lower.length - 1].toFixed(2));
 console.log('Buy signal:', buySignal.toFixed(2));
 console.log('Sell signal:', sellSignal.toFixed(2));
