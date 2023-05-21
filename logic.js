adc8ea37f3909dc91b386773dded416dd342788b
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-21 12:18:14 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -28,7 +28,7 @@ function calculateFibonacciLevels(prices, period) {
 
   return levels;
 }
-const RSI_PERIOD = 3;
+const RSI_PERIOD = 9;
 const closePrices = prices.map(price => price.close);
 const gainLosses = closePrices.map((price, i) => {
   if (i === 0) {
