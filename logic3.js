a3a7bcb4464b0a2bc184e97a9a589ae4be44f5c9
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-23 17:59:29 +1000

    Update logic3.js

--- logic3.js
+++ logic3.js
@@ -5,7 +5,7 @@ fs.readFile('price.json', (err, data) => {
   if (err) throw err;
 
   const prices = JSON.parse(data).map(candle => parseFloat(candle.close));
-  const psar = PSAR.calculate({ high: prices, low: prices, step: 0.02, max: 0.2 });
+  const psar = PSAR.calculate({ high: prices, low: prices, step: 0.05, max: 0.2 });
 
   let trend = 'none';
   let entry = 'none';
