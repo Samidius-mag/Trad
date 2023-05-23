5f2d2b5c8353ba607e9ad7c2a49084406bd70df2
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-23 18:07:34 +1000

    Update logic3.js

--- logic3.js
+++ logic3.js
@@ -5,40 +5,60 @@ fs.readFile('price.json', (err, data) => {
   if (err) throw err;
 
   const prices = JSON.parse(data).map(candle => parseFloat(candle.close));
-  const psar = PSAR.calculate({ high: prices, low: prices, step: 0.05, max: 0.2 });
+  const psar = PSAR.calculate({ high: prices, low: prices, step: 0.02, max: 0.2 });
 
   let trend = 'none';
   let entry = 'none';
   let psarPrice = 0;
   let trendPrice = 0;
 
-  if (prices[prices.length - 1] > psar[psar.length - 1]) {
-    trend = 'up';
-    psarPrice = psar[psar.length - 1];
-    if (prices[prices.length - 2] < psar[psar.length - 2]) {
-      entry = 'buy';
-      trendPrice = prices[prices.length - 1];
-    }
-  } else if (prices[prices.length - 1] < psar[psar.length - 1]) {
-    trend = 'down';
-    psarPrice = psar[psar.length - 1];
-    if (prices[prices.length - 2] > psar[psar.length - 2]) {
-      entry = 'sell';
-      trendPrice = prices[prices.length - 1];
+  for (let i = prices.length - 1; i >= 0; i--) {
+    if (prices[i] > psar[i]) {
+      trend = 'up';
+      psarPrice = psar[i];
+      if (i > 0 && prices[i - 1] < psar[i - 1]) {
+        entry = 'buy';
+        trendPrice = prices[i];
+        console.log(`Buy Entry: ${trendPrice}, PSAR Price: ${psarPrice}`);
+        break;
+      }
+    } else if (prices[i] < psar[i]) {
+      trend = 'down';
+      psarPrice = psar[i];
+      if (i > 0 && prices[i - 1] > psar[i - 1]) {
+        entry = 'sell';
+        trendPrice = prices[i];
+        console.log(`Sell Entry: ${trendPrice}, PSAR Price: ${psarPrice}`);
+        break;
+      }
     }
   }
-
-  console.log(`Trend: ${trend}, Entry: ${entry}, PSAR Price: ${psarPrice}, Trend Price: ${trendPrice}`);
 
   for (let i = prices.length - 2; i >= 0; i--) {
     if (trend === 'up' && prices[i] < psar[i]) {
       trend = 'down';
       trendPrice = prices[i];
       console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
     } else if (trend === 'down' && prices[i] > psar[i]) {
       trend = 'up';
       trendPrice = prices[i];
       console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
+    }
+  }
+
+  for (let i = prices.length - 1; i >= 0; i--) {
+    if (trend === 'down' && prices[i] > psar[i]) {
+      trend = 'up';
+      trendPrice = prices[i];
+      console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
+    } else if (trend === 'up' && prices[i] < psar[i]) {
+      trend = 'down';
+      trendPrice = prices[i];
+      console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
     }
   }
 });
