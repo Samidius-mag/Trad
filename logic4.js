bacb26c90ace87037dbfb0b500b7041aa9d929d1
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-24 13:36:30 +1000

    Update logic4.js

--- logic4.js
+++ logic4.js
@@ -1,147 +1,83 @@
 const fs = require('fs');
-const { PSAR, RSI, EMA } = require('technicalindicators');
+const { PSAR } = require('technicalindicators');
 
-const data = JSON.parse(fs.readFileSync('price.json'));
+fs.readFile('price.json', (err, data) => {
+  if (err) throw err;
 
-const close = data.map(candle => candle.close);
-const high = data.map(candle => candle.high);
-const low = data.map(candle => candle.low);
+  const prices = JSON.parse(data).map(candle => parseFloat(candle.close));
+  const psar = PSAR.calculate({ high: prices, low: prices, step: 0.02, max: 0.2 });
 
-// PSAR
-const psarInput = {
-  high,
-  low,
-  accelerationFactor: 0.02,
-  maxAccelerationFactor: 0.2,
-};
-const psar = PSAR.calculate(psarInput);
+  let trend = 'none';
+  let entry = 'none';
+  let psarPrice = 0;
+  let trendPrice = 0;
+  let reversalPrice = 0;
 
-// RSI
-const rsiInput = {
-  values: close,
-  period: 14,
-};
-const rsi = RSI.calculate(rsiInput);
-
-// EMA
-const ema21 = EMA.calculate({ period: 21, values: close });
-const ema55 = EMA.calculate({ period: 55, values: close });
-const ema89 = EMA.calculate({ period: 89, values: close });
-const ema144 = EMA.calculate({ period: 144, values: close });
-
-// Точки входа и выхода
-const buySignals = [];
-const sellSignals = [];
-
-for (let i = 1; i < data.length; i++) {
-  const prevPsar = psar[i - 1];
-  const currPsar = psar[i];
-  const prevRsi = rsi[i - 1];
-  const currRsi = rsi[i];
-  const prevEma21 = ema21[i - 1];
-  const currEma21 = ema21[i];
-  const prevEma55 = ema55[i - 1];
-  const currEma55 = ema55[i];
-  const prevEma89 = ema89[i - 1];
-  const currEma89 = ema89[i];
-  const prevEma144 = ema144[i - 1];
-  const currEma144 = ema144[i];
-
-  // Точки входа
-  if (
-    prevPsar > prevEma21 &&
-    currPsar < currEma21 &&
-    prevRsi < 30 &&
-    currRsi > 30 &&
-    prevEma21 < prevEma55 &&
-    currEma21 > currEma55 &&
-    prevEma55 < prevEma89 &&
-    currEma55 > currEma89 &&
-    prevEma89 < prevEma144 &&
-    currEma89 > currEma144
-  ) {
-    buySignals.push({
-      time: data[i].time,
-      price: data[i].close,
-    });
+  for (let i = prices.length - 1; i >= 0; i--) {
+    if (prices[i] > psar[i]) {
+      trend = 'up';
+      psarPrice = psar[i];
+      if (i > 0 && prices[i - 1] < psar[i - 1]) {
+        entry = 'buy';
+        trendPrice = prices[i];
+        console.log(`Buy Entry: ${trendPrice}, PSAR Price: ${psarPrice}`);
+        break;
+      }
+    } else if (prices[i] < psar[i]) {
+      trend = 'down';
+      psarPrice = psar[i];
+      if (i > 0 && prices[i - 1] > psar[i - 1]) {
+        entry = 'sell';
+        trendPrice = prices[i];
+        console.log(`Sell Entry: ${trendPrice}, PSAR Price: ${psarPrice}`);
+        break;
+      }
+    }
   }
 
-  // Точки выхода
-  if (
-    prevPsar < prevEma21 &&
-    currPsar > currEma21 &&
-    prevRsi > 70 &&
-    currRsi < 70 &&
-    prevEma21 > prevEma55 &&
-    currEma21 < currEma55 &&
-    prevEma55 > prevEma89 &&
-    currEma55 < currEma89 &&
-    prevEma89 > prevEma144 &&
-    currEma89 < currEma144
-  ) {
-    sellSignals.push({
-      time: data[i].time,
-      price: data[i].close,
-    });
-  }
-}
-
-console.log('Buy signals:', buySignals);
-console.log('Sell signals:', sellSignals);
-
-// Определение направления тренда
-const trendDirection = ema21[ema21.length - 1] > ema55[ema55.length - 1] ? 'up' : 'down';
-console.log('Trend direction:', trendDirection);
-
-// Уровни отскоков
-const support1 = ema55[ema55.length - 1];
-const support2 = ema89[ema89.length - 1];
-const resistance1 = ema21[ema21.length - 1];
-const resistance2 = ema144[ema144.length - 1];
-console.log('Support 1:', support1);
-console.log('Support 2:', support2);
-console.log('Resistance 1:', resistance1);
-console.log('Resistance 2:', resistance2);
-
-// Точки разворота
-const reversalPoints = [];
-for (let i = 1; i < data.length; i++) {
-  const prevEma21 = ema21[i - 1];
-  const currEma21 = ema21[i];
-  const prevEma55 = ema55[i - 1];
-  const currEma55 = ema55[i];
-  const prevEma89 = ema89[i - 1];
-  const currEma89 = ema89[i];
-  const prevEma144 = ema144[i - 1];
-  const currEma144 = ema144[i];
-
-  if (
-    prevEma21 < prevEma55 &&
-    currEma21 > currEma55 &&
-    prevEma55 < prevEma89 &&
-    currEma55 > currEma89 &&
-    prevEma89 < prevEma144 &&
-    currEma89 > currEma144
-  ) {
-    reversalPoints.push({
-      time: data[i].time,
-      price: data[i].close,
-    });
+  for (let i = prices.length - 2; i >= 0; i--) {
+    if (trend === 'up' && prices[i] < psar[i]) {
+      trend = 'down';
+      trendPrice = prices[i];
+      console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
+    } else if (trend === 'down' && prices[i] > psar[i]) {
+      trend = 'up';
+      trendPrice = prices[i];
+      console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
+    }
   }
 
-  if (
-    prevEma21 > prevEma55 &&
-    currEma21 < currEma55 &&
-    prevEma55 > prevEma89 &&
-    currEma55 < currEma89 &&
-    prevEma89 > prevEma144 &&
-    currEma89 < currEma144
-  ) {
-    reversalPoints.push({
-      time: data[i].time,
-      price: data[i].close,
-    });
+  for (let i = prices.length - 1; i >= 0; i--) {
+    if (trend === 'down' && prices[i] > psar[i]) {
+      trend = 'up';
+      trendPrice = prices[i];
+      console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
+    } else if (trend === 'up' && prices[i] < psar[i]) {
+      trend = 'down';
+      trendPrice = prices[i];
+      console.log(`Trend changed to ${trend}, Trend Price: ${trendPrice}`);
+      break;
+    }
   }
-}
 
-console.log('Reversal points:', reversalPoints);
+  if (trend === 'up') {
+    for (let i = prices.length - 1; i >= 0; i--) {
+      if (prices[i] < psar[i]) {
+        reversalPrice = prices[i];
+        console.log(`Reversal Price: ${reversalPrice}`);
+        break;
+      }
+    }
+  } else if (trend === 'down') {
+    for (let i = prices.length - 1; i >= 0; i--) {
+      if (prices[i] > psar[i]) {
+        reversalPrice = prices[i];
+        console.log(`Reversal Price: ${reversalPrice}`);
+        break;
+      }
+    }
+  }
+});
